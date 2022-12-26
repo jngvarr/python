6138460f7e69882dcb54b5b841fa6c5f5ea2ea94
Jngvarr <jngvarr@gmail.com>
2022-12-26 18:00:26 +0700

    add sem3HW4

--- sem3/Homework/4.py
+++ sem3/Homework/4.py
@@ -3,3 +3,12 @@
 # - 45 -> 101101
 # - 3 -> 11
 # - 2 -> 10
+
+num = int(input("Введите число: "))
+temp_num = num
+res = list()
+while temp_num > 0:
+    res.append(temp_num % 2)
+    temp_num //= 2
+res.reverse()
+print(f'Десятичное число {num} в двоичном виде - {"".join(map(str, res))}')
