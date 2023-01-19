290bdc882191a9033b4e805e3e0113e54a805704
Jngvarr <jngvarr@inbox.ru>
2023-01-19 23:35:08 +0700

    add string encoding

--- sem5/Homework/kross-null.py
+++ sem5/Homework/kross-null.py
@@ -12,9 +12,10 @@ win_lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
              [0, 4, 8], [2, 4, 6]]
 
 def print_pole():
+    # os.system('cls')
     for i in range(9):
         print(f'{pole[i]}\t', end="")
-        if not int(pole[i]) % 3:
+        if not (i+1) % 3:
             print('\n')
 
 def step_(step, symbol):
